e65e1db01001fe68f764d8465130149d539c464c
Stephanie Garcia <stephanie.garcia8@upr.edu>
2019-05-02 22:57:18 -0400

    Updated alu

--- alu.v
+++ alu.v
@@ -1,47 +1,47 @@
 //steph
 module alu(
            input [31:0] A,B,
-           input [5:0] opcode,
+           input [5:0] functn,
            output reg [31:0] out,
            input inputCarry,
            output reg negativeFlag, zeroFlag, carryFlag, overflowFlag
     );
     always @(*)
     begin
-        case(opcode)
-        6'b000000: // ADD
+        case (functn)
+        6'b100000: // ADD
             begin
             {carryFlag, out} = A + B;
              overflowFlag = ((A[31] == B[31]) && (A[31] != out[31])) ? 1'b1 : 1'b0;
             end
-        6'b000000: // ADDU
+        6'b100001: // ADDU
             begin
             {carryFlag, out} = A + B;
             end
         6'b001000: //ADDI
            begin
-            {carryFlag, out} = A + 16'b0000000000000000;
+            {carryFlag, out} = A + B;
             overflowFlag = ((A[31] == B[31]) && (A[31] != out[31])) ? 1'b1 : 1'b0;
             end
         6'b000101: //ADDIU
            begin
-            {carryFlag, out} = A + 16'b0000000000000000;
+            {carryFlag, out} = A + B;
             end
-        6'b000001: // SUB fixed
+        6'b100010: // SUB
             begin
             {carryFlag, out} = A - B;
             overflowFlag = (A[31] == 0 && B[31] == 1 && out[31] == 1) || (A[31] == 1 && B[31] == 0 && out[31] == 0);
             end
-        6'b000001: // SUBU
+        6'b100011: // SUBU
             begin
             {carryFlag, out} = A - B;
             overflowFlag = (A[31] == 0 && B[31] == 1 && out[31] == 1) || (A[31] == 1 && B[31] == 0 && out[31] == 0);
             end
-        6'b000001: // MULTU
+        6'b011001: // MULTU
             begin
             out = A * B;
             end
-        6'b000010: //  SLT
+        6'b101010: //  SLT
             begin
             if(A < B)
             begin
@@ -49,7 +49,7 @@ module alu(
             end
             else out = 1'b0;
             end
-        6'b000010: //  SLTU
+        6'b101011: //  SLTU
             begin
             if(A < B)
             begin
@@ -57,23 +57,23 @@ module alu(
             end
             else out = 1'b0;
             end
-        6'b000010: //  SLTI ??
+        6'b001010: //  SLTI 
             begin
-            if(A <  16'b0000000000000000)
-            begin
-                out = 1'b1;
-            end
-            else out = 1'b0;
-            end
-        6'b000010: //  SLTIU ??
-            begin
-            if(A <  16'b0000000000000000)
+            if(A < B)
             begin
                 out = 1'b1;
             end
             else out = 1'b0;
             end
-        6'b000010: //  CLO ??
+        6'b001011: //  SLTIU 
+            begin
+            if(A < B)
+            begin
+                out = 1'b1;
+            end
+            else out = 1'b0;
+            end
+        6'b100001: //  CLO 
             begin
             if(A < B)
             begin
@@ -81,7 +81,7 @@ module alu(
             end
             else out = 0;
             end
-        6'b000010: //  CLZ ??
+        6'b100000: //  CLZ 
             begin
             if(A < B)
             begin
@@ -89,47 +89,47 @@ module alu(
             end
             else out = 0;
             end
-        6'b000010: //  AND
+        6'b100100: //  AND
             begin
             out =  A & B;
             end
-        6'b000010: //  ANDI ??
+        6'b001100: //  ANDI 
             begin
-            out = A & 16'b0000000000000000;
+            out = A & B;
             end
-        6'b000011: //  OR
+        6'b100101: //  OR
             begin
             out =  A | B;
             end
-        6'b000011: //  ORI ??
+        6'b001101: //  ORI 
             begin
-            out = A |  16'b0000000000000000;
+            out = A | B;
             end
-        6'b000100: //  XOR
+        6'b100110: //  XOR
            begin
             out =  A ^ B;
             end
-        6'b000100: //  XORI ??
+        6'b001110: //  XORI 
            begin
-            out = A ^ 16'b0000000000000000;
+            out = A ^ B;
             end
-        6'b001001: // NOR
-           begin
-            out = ~(A | B);
-            end
-        6'b001001: // LUI ??
+        6'b100111: // NOR
            begin
             out = ~(A | B);
             end
-        6'b000110: // SLL
+        6'b001111: // LUI 
+           begin
+            out = ~(A | B);
+            end
+        6'b000000: // SLL
            begin
             out = A << B;
             end
-        6'b000110: // SLLV
+        6'b000100: // SLLV
           begin
             out = A << B;
             end
-        6'b000111: // SRA
+        6'b000011: // SRA
            begin
             out = A >> B;
 
@@ -139,12 +139,12 @@ module alu(
             out = A >> B;
 
             end
-        6'b000111: // SRL
+        6'b000010: // SRL
            begin
             out = A >> B;
 
             end
-        6'b000111: // SRLV
+        6'b000110: // SRLV
            begin
             out = A >> B;
             end
@@ -160,49 +160,52 @@ endmodule
 module tester;
 //Inputs
  reg[31:0] A,B;
- reg[5:0] opcode;
+ reg[5:0] functn;
  wire negativeFlag, zeroFlag, carryFlag, overflowFlag;
 
 //Outputs
  wire[31:0] out;
  integer i;
- alu test_unit(A,B,opcode,out,inputCarry,negativeFlag,zeroFlag,carryFlag,overflowFlag);
+ alu test_unit(A,B,functn,out,inputCarry,negativeFlag,zeroFlag,carryFlag,overflowFlag);
     initial begin
-
-        A = 32'h06;
-        B = 32'h0A;
+        A = 32'b10000000000000000000000000000000;
+        B = 32'b10000000000000000000000000000010;
+       // A = 32'd4294967295;
+        //B = 32'd4;
+        //A = 32'h3;
+       // B = 32'h4;
         //ADD TEST
-        opcode = 6'b000000;
+        functn = 6'b100000;
         #10;
-        #3 $display("Opcode: %b, Binary Output: %b, Decimal Output: %d, Negative Flag: %d, Zero Flag: %d, Carry Flag: %d, Overflow Flag: %d",opcode,out,out,negativeFlag,zeroFlag,carryFlag,overflowFlag);
+        #3 $display("A: %b, B: %b,Function: %b, Binary Output: %b, Decimal Output: %d, Negative Flag: %b, Zero Flag: %d, Carry Flag: %d, Overflow Flag: %d",A,B,functn,out,out,negativeFlag,zeroFlag,carryFlag,overflowFlag);
         #10;
-        //SUB TEST
-        opcode = 6'b000001;
-          #3 $display("Opcode: %b, Binary Output: %b, Decimal Output: %d, Negative Flag: %d, Zero Flag: %d, Carry Flag: %d, Overflow Flag: %d",opcode,out,out,negativeFlag,zeroFlag,carryFlag,overflowFlag);
-        #10;
-        //AND TEST
-        opcode = 6'b000010;
-        #3 $display("Opcode: %b, Binary Output: %b, Decimal Output: %d, Zero Flag: %d",opcode,out,out,zeroFlag);
-        #10;
-        //OR TEST
-        opcode = 6'b000011;
-        #3 $display("Opcode: %b, Binary Output: %b, Decimal Output: %d, Zero Flag: %d",opcode,out,out,zeroFlag);
-        #10;
-        //XOR TEST
-        opcode = 6'b000100;
-       #3 $display("Opcode: %b, Binary Output: %b, Decimal Output: %d, Zero Flag: %d",opcode,out,out,zeroFlag);
-        #10;
-        //NOR TEST
-        opcode = 6'b001001;
-      #3 $display("Opcode: %b, Binary Output: %b, Decimal Output: %d, Zero Flag: %d",opcode,out,out,zeroFlag);
-        #10;
-        //SLL TEST
-        opcode = 6'b000110;
-        #3 $display("Opcode: %b, Binary Output: %b, Decimal Output: %d, Zero Flag: %d",opcode,out,out,zeroFlag);
-        #10;
-         //SRL TEST
-        opcode = 6'b000111;
-         #3 $display("Opcode: %b, Binary Output: %b, Decimal Output: %d, Zero Flag: %d",opcode,out,out,zeroFlag);
-        #10;
+    //     //SUB TEST
+    //      functn = 6'b000001;
+    //       #3 $display(   functn: %b, Binary Output: %b, Decimal Output: %d, Negative Flag: %d, Zero Flag: %d, Carry Flag: %d, Overflow Flag: %d"  functn,out,out,negativeFlag,zeroFlag,carryFlag,overflowFlag);
+    //     #10;
+    //     //AND TEST
+    //      functn = 6'b000010;
+    //     #3 $display( functn: %b, Binary Output: %b, Decimal Output: %d, Zero Flag: %d"  functn,out,out,zeroFlag);
+    //     #10;
+    //     //OR TEST
+    //      functn = 6'b000011;
+    //     #3 $display( functn: %b, Binary Output: %b, Decimal Output: %d, Zero Flag: %d"  functn,out,out,zeroFlag);
+    //     #10;
+    //     //XOR TEST
+    //      functn = 6'b000100;
+    //    #3 $display(  functn: %b, Binary Output: %b, Decimal Output: %d, Zero Flag: %d"   functn,out,out,zeroFlag);
+    //     #10;
+    //     //NOR TEST
+    //      functn = 6'b001001;
+    //   #3 $display(   functn: %b, Binary Output: %b, Decimal Output: %d, Zero Flag: %d"    functn,out,out,zeroFlag);
+    //     #10;
+    //     //SLL TEST
+    //      functn = 6'b000110;
+    //     #3 $display( functn: %b, Binary Output: %b, Decimal Output: %d, Zero Flag: %d"  functn,out,out,zeroFlag);
+    //     #10;
+    //      //SRL TEST
+    //      functn = 6'b000111;
+    //      #3 $display(    functn: %b, Binary Output: %b, Decimal Output: %d, Zero Flag: %d" functn,out,out,zeroFlag);
+    //     #10;
     end
 endmodule
